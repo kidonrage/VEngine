eb3dc61caab3787aa5e98db09fe314e673c2aec0
Vlad Eliseev <freshymanchan@gmail.com>
2020-04-10 23:15:03 +1000

    Initial Commit

---
+++ VEngine/VEngine.h
@@ -0,0 +1,19 @@
+//
+//  VEngine.h
+//  VEngine
+//
+//  Created by Vlad Eliseev on 10.04.2020.
+//  Copyright © 2020 Vlad Eliseev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VEngine.
+FOUNDATION_EXPORT double VEngineVersionNumber;
+
+//! Project version string for VEngine.
+FOUNDATION_EXPORT const unsigned char VEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VEngine/PublicHeader.h>
+
+
